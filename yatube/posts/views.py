22a3f58cec470f8b08735d6e5607b0d4bd4bcdda
RUS_Next <m.bychenkov@gmail.com>
2025-06-28 23:59:28 +0300

    Added html-pages

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Main page")
+    template = 'posts/index.html'
+    return render(request, template)
 
 def group_posts(request, slug=None):
-    return HttpResponse(f"Group posts {slug}")
+    template = 'posts/group_list.html'
+    return render(request, template)
